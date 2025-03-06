6b8b901f4982b9b8652a269a94a190a784cb769c
Ziad AMR <zezoamr190@gmail.com>
2025-03-06 19:11:23 +0200

    Fix: Remove unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/provider/theme-provider";
