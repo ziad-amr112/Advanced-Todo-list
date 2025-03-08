e6a27414218a7e136ce1e8c3f76d73ec90b1a2fc
Ziad AMR <zezoamr190@gmail.com>
2025-03-08 02:09:29 +0200

    Fix hydration error and improve component rendering

--- components/TaskWrapper.tsx
+++ components/TaskWrapper.tsx
@@ -1,9 +1,9 @@
-"use client";
+"use client"
 
+import { useEffect, useState, useCallback } from "react";
+import { useSearchParams } from "next/navigation";
 import { getTodo } from "@/actions/todoActions";
 import Tasks from "@/components/Tasks";
-import { useEffect, useCallback, useState } from "react";
-import { useSearchParams } from "next/navigation";
 import { useTodoStore } from "@/app/store/todoStore";
 
 interface TaskWrapperProps {
@@ -12,34 +12,41 @@ interface TaskWrapperProps {
 
 export default function TaskWrapper({ userId }: TaskWrapperProps) {
   const searchParams = useSearchParams();
-  const pageFromUrl = Number(searchParams.get("page")) || 1;
+  const [pageFromUrl, setPageFromUrl] = useState<number>(1);
 
   const { todos, setTodos, totalTodos, setTotalTodos } = useTodoStore();
 
   const fetchTodos = useCallback(async () => {
-    if (!userId) return; 
+    if (!userId) return;
     try {
       const { todos, totalTodos } = await getTodo({ userId, page: pageFromUrl });
       setTodos(todos);
       setTotalTodos(totalTodos);
     } catch (error) {
       console.error("Failed to fetch todos:", error);
-    } finally {
     }
   }, [userId, pageFromUrl, setTodos, setTotalTodos]);
 
   useEffect(() => {
+    const page = Number(searchParams.get("page"));
+    if (!isNaN(page) && page > 0) {
+      setPageFromUrl(page);
+    }
+  }, [searchParams]);
+
+  useEffect(() => {
     fetchTodos();
-  }, [fetchTodos]); 
+  }, [fetchTodos, pageFromUrl]); 
+
   return (
-    <div className="flex justify-center">
-        <Tasks
-          todos={todos}
-          userId={userId}
-          totalTodos={totalTodos}
-          currentPage={pageFromUrl}
-          refetchTodos={fetchTodos}/>
-      
+    <div suppressHydrationWarning={true} className="flex justify-center">
+      <Tasks
+        todos={todos}
+        userId={userId}
+        totalTodos={totalTodos}
+        currentPage={pageFromUrl}
+        refetchTodos={fetchTodos}
+      />
     </div>
   );
 }
